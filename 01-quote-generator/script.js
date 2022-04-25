7ee92eed4f43c0150e9a9a0c42b696afda4cbb37
hyxfish27 <avicky1515@gmail.com>
2022-04-26 06:56:57 +0800

    收藏功能微調

--- 01-quote-generator/script.js
+++ 01-quote-generator/script.js
@@ -1,23 +1,31 @@
-const container = document.getElementById('container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
+const container = document.querySelector('#container');
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const authorText = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const favoritedBtn = document.querySelector('.fav-button');
+const showFavorteBtn = document.querySelector('#show-favorite');
+const favoriteContainer = document.querySelector('.favorite-container');
+const newQuoteBtn = document.querySelector('#new-quote');
+const loader = document.querySelector('#loader');
 
 
 let apiQuotes = [];
+let tempQuote = "";
+
 
 //Show Loading
 function loading() {
     loader.hidden = false;
-    container.hidden = true;
+    quoteContainer.hidden = true;
+    showFavorteBtn.hidden = true;
 }
 
 //Hide Loading
 function complete() {
-    container.hidden = false;
     loader.hidden = true;
+    quoteContainer.hidden = false;
+    showFavorteBtn.hidden = false;
 }
 
 // Show New Quote 
@@ -26,7 +34,7 @@ function newQuote() {
     // Pick a random quote from apiQuotes array
     index = Math.floor(Math.random() * apiQuotes.length);
     const quote = apiQuotes[index];
-    //Check if Author field is blank and replace it with 'Unkown'
+    // Check if Author field is blank and replace it with 'Unkown'
     if (!quote.author) {
         authorText.textContent = 'Unkown';
     } else {
@@ -54,6 +62,76 @@ async function getQuotes() {
     } catch (error) {
         // Catch Error Here
     }
+    complete();
+}
+
+// Get Favorite Qoutes from LocalStorage
+function getFavorites() {
+    tempQuote = JSON.parse(localStorage.getItem('liked_quote')) || [];
+    let template = ''
+    tempQuote.forEach(quote => {
+        template +=
+            `<div class="fav-list">
+                <button class="remove-btn" data-id=${quote.time}>
+                <i class="fa-solid fa-x"></i>
+                </button>
+                <p class="favorite-content">${quote.text} -- [ ${quote.author} ]</p>
+            </div>`;
+
+    })
+    favoriteContainer.innerHTML = template;
+    const removeBtn = document.querySelectorAll('.remove-btn');
+    removeBtn.forEach(btn => {
+        btn.addEventListener('click', function () {
+            let target = parseInt(btn.dataset.id);
+            removeFavorite(target);
+        });
+    })
+}
+
+// Show Favorite Quotes
+function showFavorite() {
+    if (favoriteContainer.classList.contains('hidden')) {
+        favoriteContainer.classList.remove('hidden')
+    } else {
+        favoriteContainer.classList.add('hidden')
+    }
+}
+
+function removeFavorite(id) {
+    let index = tempQuote.findIndex(temp => temp.time === id);
+    tempQuote.splice(index, 1);
+    localStorage.setItem('liked_quote', JSON.stringify(tempQuote));
+    getFavorites();
+}
+
+function addFavorite(quote){
+    tempQuote.push(quote)
+    favoritedBtn.classList.add('liked')
+}
+
+function toggleFavorite() {
+    const time = new Date().getTime()
+    const quote = {
+        text: quoteText.innerText,
+        author: authorText.innerText,
+        time: time
+    }
+    let index = -1;
+
+    if (tempQuote && !tempQuote.length) {
+        tempQuote.push(quote)
+    } else {
+        tempQuote.forEach((temp, i) => {
+            if (temp.text === quote.text) {
+                index = i;
+            }
+        });
+        index === -1 ? addFavorite(quote) : removeFavorite(index);
+    }
+    localStorage.setItem('liked_quote', JSON.stringify(tempQuote));
+    getFavorites();
+
 }
 
 // Tweet Quote
@@ -64,10 +142,11 @@ function tweetQuote() {
 
 //Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote); 
+twitterBtn.addEventListener('click', tweetQuote);
+favoritedBtn.addEventListener('click', toggleFavorite);
+showFavorteBtn.addEventListener('click', showFavorite);
 
 // On Load
 getQuotes();
-//newQuote();
-
+getFavorites();
 
